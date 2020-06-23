3721bf78f61a17004a72be444cc806341d0ff916
Mohamed Naceur Souissi <nacer.suissi@gmail.com>
2020-06-23 16:34:16 +0100

    starting class

---
+++ file.py
@@ -0,0 +1,11 @@
+class Pet :
+    def __init__(self,name,type,color):
+        self.name  = name
+        self.type=type
+        self.color=color
+
+p=Pet("zebi","cat","blue")
+
+print (p.name)
+print (p.type)
+print (p.color)
